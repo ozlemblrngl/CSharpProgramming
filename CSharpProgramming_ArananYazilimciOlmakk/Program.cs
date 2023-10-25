e6294db908fc3a4dc94c0e2f30dc797b99445fb5
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-10-25 21:49:52 +0300

    new project added

---
+++ CSharpProgramming_ArananYazilimciOlmakk/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /********************* İYİ BİR YAZILIMCI OLMAK İÇİN GEREKENLER *************************************
+         * 
+         * 
+        1 - Çevik kodlama yapmak,
+        kodun değişime direnç göstermemesi ve nesnel olmasıdır. nesnellik ise yazılan kodların değişikliklere direnç göstermemesidir.
+        Sürdürülebilir kod yazarsak, gelen değişiklik taleplerine karşılık verebiliriz.
+        Çevik kodlamanın yöntemi de SOLID 'tir.Kodlama yaparken SOLID'e aykırılık yapıyor muyuz diye düşünerek hareket etmeliyiz.
+        iyi programcı ileride olacakları bilmeden biliyormuş gibi kodlayan kişidir.
+
+        2 - Code Review 
+        Yazılımcı belli aralıklarla kodlarını gözden geçirmelidir ve bu aralıkların sık olması gerekir.
+        örn 8 saat kod yazacaksın her 2 saatte bir 10 dklik bir review zamanı koy. her ne kadar vakit harcıyomuşsun gibi görünse de uzun vadede zaman kazandırıyor.
+        Eğer yazılım mimarisi kötüyse buna CODE SMELL yani kötü kokan kod denir.Review ederek bunu azaltırız. Yaptıklarımızı gözden geçirip kodu iyileştirmemiz gerekir.
+        SOLID'e aykırılık varsa bunları düzeltmemiz gerekir bu sırada.Nesnel olmayan, kod tekrarı olan durumlar varsa onları kaldırmamız gereklidir.
+
+        3- Takım oyununu sever, kurumsal hafızaya zarar vermez.
+        İyi bir geliştirici yarışa girmez, takım arkadaşlarının da kalkınması için uğraşır. Takım arkadaşımız ne kadar gelişirse, bizler de o oranda gelişiriz.
+        Kurumsal hafıza bilginin bireylerde değil, kurumlafda kalmasıdır.
+
+        4- Sosyal Bilimler Önemser
+        Sosyoloji, felsefe ve psikoloji çok önemlidir. Bir yazılımcı sosyal bilimleri de işin içine katarsa o yazılımcının yapamayacağı şey yoktur.
+        İleitişim yazılımcı için çok önemlidir. Müşteriyi doğru anlamak ve taleplerine doğru karşılık vermek çok önemlidir. Program ne kadar iyi olursa olsun talebi karşılamazsa önemi olmaz.
+         */
+    }
+}
