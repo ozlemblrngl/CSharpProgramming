8c69a7a2f5d2e85da5ab97a32f50b0d69ea1f508
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-10-25 22:48:55 +0300

    item added

--- CSharpProgramming_SOLID_YazilimGelistirmePrensipleri/Program.cs
+++ CSharpProgramming_SOLID_YazilimGelistirmePrensipleri/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+       /******************* SOLID ************************************************
+       
+       Yazılım geliştirici isek SOLID'e hakim olmamız gerekiyor. Solid ne sağlar? Yazılımda sürdürülebilirlik ve Clean Code dediğimiz süreçleri kapsar.
+       Sürdürülebilir olmanın yazılımda karşılığı şudur, değişiklik yapılması gerektiğinde direnç göstermemektir.
+
+       SOLID 5 adet prensibin ilk harflerinden oluşur.
+
+       1- Single responsibity
+       Tek sorumluluk; yazılım geliştirirken, her bloğun, her classın, her metodun, her katmanın vs sadece ve sadece bir görevi olmalıdır.
+       Hiçbir şekilde bir tane metodun içinde iki tane iş yapılmaz. Bir classın içerisinde iki farklı nesne yönetilmez. Bir katmanda o katmanın dışında iş yapılmaz.
+       Mvc de sadece arayüzü ilgilendiren kodlar yazılır. 
+
+
+
+       2- Open Closed
+       Programlar yeni özellikler, değişiklikler eklemeye açık fakat mevcut özellikleri değiştirmeye kapalı olmalıdır. Ysni projeye yeni bir özellik eklediğimizde, 
+       mevcuttaki tek bir satır kodu dahi değiştiremeyiz.
+
+
+       3- Liskov's Substitution
+       4- Interface Segregation
+       5- Dependency Inversion
+
+
+       
+
+
+
+        */
+    }
+}
---
+++ CSharpProgramming_SOLID_YazilimGelistirmePrensipleri/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming_SOLID_YazilimGelistirmePrensipleri
+{
+
+    // ********************************* AŞAĞIDA SINGLE RESPONSIBILITY İLKESİNE AYKIRILIK GÖSTERİLMİŞTİR ******************************
+
+    class CustomerManager
+    {
+
+        // Aspect Orieented Programming burada devreye giriyor.
+        public void TransactionalOperation()
+        {
+            // loglama
+            // validation
+            // caching
+            // Authorization kodu
+            // Performance check 
+            // programcı burada yukarıdaki işlemleri yapabiliyor bu durumda single responsibility kuralını bir güzel çiğnemiş oluyor. Bunlar yapılmaması gereken şeyler.
+
+            Insert();
+            Update(); // doğru şekli budur.Aşağıdaki gibi ayrı metodlarda yazıp buraya almak.
+
+            //MyContext context = new MyContext();
+            //context.Update();
+            //context.Insert();
+            // (yukarıdaki 3 satır)Bir metodun içerisinde iki ayrı görev yüklenmesi yazdığımız kodda sıkıntı çıkarıyor. Aşağıdaki sınıfta daha net görülüyor. Bu durum single responsibility ilkesine uygun değil.
+
+        }
+
+        // Yukarıdaki metot yerine doğru olarak yazılacak metot kodları aşağıdaki gibidir ve her biri birbirinden ayrı görevi tekil olarak üstlenmektedir. Doğrusu da budur.
+
+        public void Insert()
+        {
+            MyContext context = new MyContext();
+            context.Insert();
+        }
+
+        public void Update()
+        {
+            MyContext context = new MyContext();
+            context.Update();
+
+        }
+
+    }
+
+    class PersonManager
+    {
+        public void Add()
+        {
+            CustomerManager customerManagar = new CustomerManager();
+
+            // customerManagar.TransactionalOperation();  
+
+            // (tek yorum satırı) Yukarıda TransactionalOpration metodunda hem update hem insert iki ayrı metodu kullandığımız için eğer sadece insert edeceksek burada sıkıntı yaşıyoruz. 
+            // ekleme fonksiyonunda sadece insert i kullanmak isterken aynı metoda iki ayrı görev yüklemiş olmamız nedeniyle sıkıntı oluyor.
+            // bu durumda SOLID'in single Responsibility ilkesine aykırılık teşkil ediyor.
+
+            // doğru hali yukarıda ayrı metotlara alınmış hali doğrultusunda sadece insert etme amaçlı olarak aşağıdaki gibidir.
+
+            customerManagar.Insert();
+        }
+    }
+
+    class MyContext
+    {
+        public void Update()
+        {
+
+        }
+
+        public void Insert()
+        {
+
+        }
+    }
+}
