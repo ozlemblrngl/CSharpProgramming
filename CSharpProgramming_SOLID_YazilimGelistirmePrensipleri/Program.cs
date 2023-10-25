ad9aab04a35a0fe9b806185e83c798ef22ce417f
ozlemblrngl <av.ozlembelorenoglu@gmail.com>
2023-10-26 00:52:00 +0300

    last changes added about dip

--- CSharpProgramming_SOLID_YazilimGelistirmePrensipleri/Program.cs
+++ CSharpProgramming_SOLID_YazilimGelistirmePrensipleri/Program.cs
@@ -2,33 +2,36 @@
 {
     private static void Main(string[] args)
     {
-       /******************* SOLID ************************************************
-       
-       Yazılım geliştirici isek SOLID'e hakim olmamız gerekiyor. Solid ne sağlar? Yazılımda sürdürülebilirlik ve Clean Code dediğimiz süreçleri kapsar.
-       Sürdürülebilir olmanın yazılımda karşılığı şudur, değişiklik yapılması gerektiğinde direnç göstermemektir.
+        /******************* SOLID ************************************************
 
-       SOLID 5 adet prensibin ilk harflerinden oluşur.
+        Yazılım geliştirici isek SOLID'e hakim olmamız gerekiyor. Solid ne sağlar? Yazılımda sürdürülebilirlik ve Clean Code dediğimiz süreçleri kapsar.
+        Sürdürülebilir olmanın yazılımda karşılığı şudur, değişiklik yapılması gerektiğinde direnç göstermemektir.
 
-       1- Single responsibity
-       Tek sorumluluk; yazılım geliştirirken, her bloğun, her classın, her metodun, her katmanın vs sadece ve sadece bir görevi olmalıdır.
-       Hiçbir şekilde bir tane metodun içinde iki tane iş yapılmaz. Bir classın içerisinde iki farklı nesne yönetilmez. Bir katmanda o katmanın dışında iş yapılmaz.
-       Mvc de sadece arayüzü ilgilendiren kodlar yazılır. 
+        SOLID 5 adet prensibin ilk harflerinden oluşur.
 
-
-
-       2- Open Closed
-       Programlar yeni özellikler, değişiklikler eklemeye açık fakat mevcut özellikleri değiştirmeye kapalı olmalıdır. Ysni projeye yeni bir özellik eklediğimizde, 
-       mevcuttaki tek bir satır kodu dahi değiştiremeyiz.
+        1- Single responsibity
+        Tek sorumluluk; yazılım geliştirirken, her bloğun, her classın, her metodun, her katmanın vs sadece ve sadece bir görevi olmalıdır.
+        Hiçbir şekilde bir tane metodun içinde iki tane iş yapılmaz. Bir classın içerisinde iki farklı nesne yönetilmez. Bir katmanda o katmanın dışında iş yapılmaz.
+        Mvc de sadece arayüzü ilgilendiren kodlar yazılır. 
 
 
-       3- Liskov's Substitution
-       İki şey sırf birbirine benziyor diye birbirinin yerine kullanılmamalı.
 
-       4- Interface Segregation
-       Her şeyi yani operasyonu interface içine koymamalıyız, nerede nasıl kullanılacaklarını öngörerek ayrı interfaceler oluşturmalı.
+        2- Open Closed
+        Programlar yeni özellikler, değişiklikler eklemeye açık fakat mevcut özellikleri değiştirmeye kapalı olmalıdır. Ysni projeye yeni bir özellik eklediğimizde, 
+        mevcuttaki tek bir satır kodu dahi değiştiremeyiz.
 
-       5- Dependency Inversion
 
-        */
+        3- Liskov's Substitution
+        İki şey sırf birbirine benziyor diye birbirinin yerine kullanılmamalı.
+
+        4- Interface Segregation
+        Her şeyi yani operasyonu interface içine koymamalıyız, nerede nasıl kullanılacaklarını öngörerek ayrı interfaceler oluşturmalı.
+
+        5- Dependency Inversion
+        Yani, Dependency Inversion Principle (DIP) temel olarak üst seviye modüllerin alt seviyeli detaylardan (concrete implementations) ziyade 
+        soyutlamalara (abstract interfaces) dayanması gerektiğini belirtir.
+        Bu sayede sistemde yapılan değişikliklerin diğer modüllere olan etkisi minimize edilmiş olur.
+
+         */
     }
 }
